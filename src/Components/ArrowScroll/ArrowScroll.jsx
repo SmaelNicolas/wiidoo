3c6ebd522678db8b321774a43021c753e57da96e
Smael Nicolas <nico.smael@gmail.com>
2022-06-27 15:01:31 -0300

    scroll arrows

---
+++ src/Components/ArrowScroll/ArrowScroll.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./arrowScroll.css";
+
+export const ArrowScroll = () => {
+	return (
+		<div className='arrow'>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	);
+};
