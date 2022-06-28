5b96fb014d045ac329a6b715847909854b7a7d63
Smael Nicolas <nico.smael@gmail.com>
2022-06-28 20:39:11 -0300

    add wtsp button

---
+++ src/Components/WhatsApp/WhatsApp.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import imgWhatsapp from "../../Assets/Imgs/wtspgreen.svg";
+import { openInNewTab } from "../../Helpers/openInNewTab";
+
+import "./whatsApp.css";
+
+export const WhatsApp = () => {
+	return (
+		<img
+			src={imgWhatsapp}
+			alt='wtsp redirect'
+			onClick={() =>
+				openInNewTab("https://api.whatsapp.com/send/?phone=")
+			}
+			className='whatsAppButton'
+		/>
+	);
+};
