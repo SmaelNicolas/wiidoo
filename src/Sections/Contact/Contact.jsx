49933878cce60e7555bdf289bdb1611e947cb763
Smael Nicolas <nico.smael@gmail.com>
2022-06-29 19:53:35 -0300

    add components

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Form } from "../../Components/Form/Form";
+import { IconMedia } from "../../Components/IconMedia/IconMedia";
 import "./contact.css";
 
 export const Contact = () => {
 	return (
 		<div className='contact' id='contact'>
+			<h2 className='contact--title'>¡Contacto!</h2>
+			<IconMedia />
 			<Form />
 		</div>
 	);
