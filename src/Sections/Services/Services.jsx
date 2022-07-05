16cb40f13c477673ff3aba8bacde7291a073f12e
Smael Nicolas <nico.smael@gmail.com>
2022-07-05 00:26:46 -0300

    change color, add img

--- src/Sections/Services/Services.jsx
+++ src/Sections/Services/Services.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { CardServices } from "../../Components/CardServices/CardServices";
 import "./services.css";
 import { FaLaptopCode } from "react-icons/fa";
-import { FaUniversalAccess } from "react-icons/fa";
 import { BsSearch } from "react-icons/bs";
 import { MdDevices } from "react-icons/md";
 import { MdEmail } from "react-icons/md";
@@ -11,81 +10,66 @@ import { MdDomainVerification } from "react-icons/md";
 import { BsServer } from "react-icons/bs";
 import { BsLock } from "react-icons/bs";
 import { ImStatsDots } from "react-icons/im";
+import { SiGooglemybusiness } from "react-icons/si";
+import Serv from "../../Assets/Imgs/serv.gif";
 
 export const Services = () => {
 	return (
 		<div className='services' id='services'>
-			{/* <h2 className='services--title'>Servicios</h2> */}
-			<div className='serviciosBg'>
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-				SERVICIOS SERVICIOS SERVICIOS SERVICIOS
-			</div>
+			<h2 className='title'>Servicios</h2>
 			<div className='services--card--cointainer'>
 				<CardServices
 					img={<FaLaptopCode />}
 					title='Desarrollo'
-					body='Creamos tu sitio web desde 0 , sin plantillas. Totalmente personalizado a tus necesidades.'
+					body='Creamos tu sitio web desde 0 , sin plantillas. Totalmente personalizado a tus necesidades y gustos.'
 				/>
 				<CardServices
 					img={<BsSearch />}
 					title='Seo'
-					body='Optimizar el contenido del sitio y obtener más visibilidad y clasificar más alto en los motores de búsqueda. '
-				/>
-				<CardServices
-					img={<FaUniversalAccess />}
-					title='Accesibilidad'
-					body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-					et ipsam, deleniti ipsa natus quaerat.'
+					body='Mejorar la posición de un sitio web en los motores de búsqueda para que los visitantes puedan llegar a través de búsquedas relevantes.'
 				/>
 				<CardServices
 					img={<MdDevices />}
 					title='responsive'
-					body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-					et ipsam, deleniti ipsa natus quaerat.'
+					body='Desarrollado para adaptarse a todos los dispositivos, como Smartphones y tablets, importante para alcanzar un público mayor.'
 				/>
 				<CardServices
 					img={<MdDomainVerification />}
 					title='dominio'
-					body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-					et ipsam, deleniti ipsa natus quaerat.'
+					body='Los dominio permiten crear tu propia identidad en Internet. Nosotros nos encargamos de registrarlo. '
 				/>
 				<CardServices
 					img={<BsServer />}
 					title='host'
-					body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-					et ipsam, deleniti ipsa natus quaerat.'
+					body='Hosting que se ajusta al trafico del sitio. Sin publicidades invasivas y comlpetamente seguro.'
 				/>
 				<CardServices
 					img={<BsLock />}
 					title='Seguridad'
-					body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-					et ipsam, deleniti ipsa natus quaerat.'
+					body='Certificados SSL/TLS , para que tu web sea mas segura.'
 				/>
 				<CardServices
 					img={<MdEmail />}
 					title='Email Marketing'
-					body='Envia correos electrónicos efectivos y hace crecer tu audiencia leal.'
+					body='Enviamos correos electrónicos efectivos para hacer crecer tu audiencia leal.'
 				/>
 				<CardServices
 					img={<ImStatsDots />}
 					title='Google Analytics'
-					body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-					et ipsam, deleniti ipsa natus quaerat.'
+					body='Monitoreá y analizá el perfil de quién accede a tu sitio, las páginas más accedidas, dispositivos, ciudades y muchos datos más.'
 				/>
 				<CardServices
 					img={<MdScreenSearchDesktop />}
 					title='Google search'
-					body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-					et ipsam, deleniti ipsa natus quaerat.'
+					body='Monitoreá, mantené y soluciona problemas relacionados con la presencia del sitio en los resultados de búsqueda de Google.'
+				/>
+				<CardServices
+					img={<SiGooglemybusiness />}
+					title='Google Business'
+					body='Creamos tu ficha en Google My Business con los detalles de tu negocio para hacer que destaques y conviertas esas búsquedas en clientes.'
 				/>
 			</div>
+			<img className='services--img' src={Serv} alt='servicios web' />
 		</div>
 	);
 };
