ebf3614108195f508bf722b7b22851cbb658122d
Smael Nicolas <nico.smael@gmail.com>
2022-07-05 00:28:15 -0300

    add aboutus section

---
+++ src/Sections/AboutUs/AboutUs.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { FcIdea } from "react-icons/fc";
+
+import Hand from "../../Assets/Imgs/hand.gif";
+import Us from "../../Assets/Imgs/us.gif";
+
+import "./aboutUs.css";
+
+export const AboutUs = () => {
+	return (
+		<div className='aboutUs' id='us'>
+			<h2 className='title'>Nosotros</h2>
+			<div className='aboutUs--content'>
+				En <span className='aboutUs--content--name'>WiiDoo</span> nos
+				enfocamos en la creación de sitios webs útiles para
+				emprendedores, locales gastronómicos, profesionales
+				independientes, servicios inmobiliarios y más!
+			</div>
+			<div className='aboutUs--content'>
+				¿Queres mostrar tus productos? ¿Ofrecer tus servicios? ¿Un Menú
+				para tu local? no dudes en escribirnos por cualquier medio.
+				Estamos para ayudarte a decidir lo mejor para vos y llevar tu
+				<FcIdea className='aboutUs--content--idea' />a la web!
+			</div>
+			<div className='aboutUs--content'>
+				<a className='aboutUs--content--link' href='#contact'>
+					¡Quiero mi WEB
+					<img
+						className='aboutUs--content--hand'
+						src={Hand}
+						alt='hola! quiero mi web'
+					/>
+					!
+				</a>
+			</div>
+
+			<img className='aboutUs--image' src={Us} alt='Hacemos webs' />
+		</div>
+	);
+};
